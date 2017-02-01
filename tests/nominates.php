a95dcf8fff3eae58df302cf72941eae911c80fbe
omer1714 <omerjavedcheema@gmail.com>
2017-02-01 14:37:47 -0800

    Test files sync (not working)

--- tests/nominates.php
+++ tests/nominates.php
@@ -4,14 +4,9 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class nominates extends Eloquent
+class nominates extends testCase
 {
     protected $fillable = ['snum', 'sfirstname', 'slastname']
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
     public function findsnum($query)
     {
         return $query->where('snum', '==', 41841099)
