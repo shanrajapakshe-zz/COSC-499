cd4af7439f0346c6342fc7d97e3a9828469d9b27
omer1714 <omerjavedcheema@gmail.com>
2017-02-05 12:49:20 -0500

    Changed the Database from memory to localhost mySQL. We can also add a table for testing purposes and try adding into table in local DB

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class DatabaseTest extends TestCase
 {
+		use DatabaseTransactions;
+
 		public function setUp()
 		{
 			parent::setUp();
@@ -14,7 +16,7 @@ class DatabaseTest extends TestCase
 		}
 
 		#USE DatabaseMigrations;
-	/**
+		/**
      * A basic test example.
      *
      * @return void
@@ -31,6 +33,12 @@ class DatabaseTest extends TestCase
 
 		$this->visit('/profile')
              ->see('Contact');
+
+		$this->seeInDatabase('nomination', ['studentNumber' => '12345678']);
+
+		$this->seeInDatabase('nomination', ['studentFirstName' => 'John']);
+
+		$this->seeInDatabase('nomination', ['email' => 'bon@jovi.com']);
 
 		#$this->assertTrue($nomination
 			 #->has('John'));
@@ -64,12 +72,12 @@ class DatabaseTest extends TestCase
 		#		php artisan generate:migration create_nominations_table --fields="snum:integer, sfirstname:string, slastname:string"
 		#This is not necessary (as a function is made to take care of the database in memory)
 		#		php artisan migrate --env=testing
-		nominates::create(['snum' => '41841099', 'sfirstname' => 'Omer', 'slastname' => 'Cheema']);
-		nominates::create(['snum' => '31284920', 'sfirstname' => 'John', 'slastname' => 'Doe']);
+		#nominates::create(['snum' => '41841099', 'sfirstname' => 'Omer', 'slastname' => 'Cheema']);
+		#nominates::create(['snum' => '31284920', 'sfirstname' => 'John', 'slastname' => 'Doe']);
 
-		$nominations = nominates::findsnum()->get();
+		#$nominations = nominates::findsnum()->get();
 
-		$this->assertCount(1, $nominations);
+		#$this->assertCount(1, $nominations);
 
     }
 }
