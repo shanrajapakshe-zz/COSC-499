639b364f3eb97f02f1e2f9bb8cfdab92da3ae33a
Shan Rajapakshe <shan.rajapakshe@gmail.com>
2017-02-07 16:44:21 -0800

    Changes for access to remote database

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 's97.ok.ubc.ca'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'cap499_awards'),
+            'username' => env('DB_USERNAME', 'cap499_awards'),
+            'password' => env('DB_PASSWORD', '3CdVC2dg'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
