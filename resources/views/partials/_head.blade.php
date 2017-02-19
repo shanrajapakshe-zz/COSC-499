61528e04a0b3178210199ccc4e6bd1f595341583
arsalank25 <arsalank25@gmail.com>
2017-02-19 11:38:32 -0800

    the admin nom page now has an alluring nav bar that can sort/order search best thing since the invention of prime time TV

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -6,6 +6,8 @@
 <title>@yield('title')</title> <!-- CHANGE THIS TITLE FOR EACH PAGE -->
 
 <!-- Bootstrap -->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
