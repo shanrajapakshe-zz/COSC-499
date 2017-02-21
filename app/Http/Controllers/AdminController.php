04694c9ce63fcc7164f47714de72ef5c37aba814
Shan Rajapakshe <shan.rajapakshe@gmail.com>
2017-02-20 19:51:27 -0800

    fix bug on deployment server

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Nomination;
 use App\Award;
 use App\Prof;
-use App\course;
+use App\Course;
 use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
