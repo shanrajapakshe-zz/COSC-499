c9ed170b3e07a31132c1b9de5fdda148cde637a1
Brandon Truong <Brandon.t1995@hotmail.com>
2017-03-21 14:36:05 -0700

    change password for gmail acct

--- resources/views/admin/emailMessage.blade.php
+++ resources/views/admin/emailMessage.blade.php
@@ -1,6 +1,7 @@
 Dear Student, 
 
-	You have been nominated for a Unit 5 Award. We are holding a reception to celebrate your achievements. Please attend at (place + time).
+	You have been nominated for a Unit 5 Award. We are holding a reception to celebrate your achievements. Please attend at (place + time). Edit
 
-Sincerely, 
+Sincerely,  123
 	Unit 5 Award Committee 
+
