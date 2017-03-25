800e3cfc7b83051aae030bd6832121960b8e3c6e
arsalank25 <arsalank25@gmail.com>
2017-03-24 20:43:18 -0700

    home button removed from nav and spelling in about tab fixed

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -7,7 +7,7 @@
         <div class="row">
             <div class="col-md-12">
                 <h1>About Unit 5</h1>
-                <p>Unit 5 is part of the University of Brisch Columbia Okanagan's (UBCO) Irving K. Barber School of Arts and Sciences. Composed of 4 seperate disciplines, it prides itself on being an innovater in the many fields of quantitative sciences. </p>
+                <p>Unit 5 is part of the University of British Columbia Okanagan's (UBCO) Irving K. Barber School of Arts and Sciences. Composed of 4 seperate disciplines, it prides itself on being an innovater in the many fields of quantitative sciences. </p>
                 <ul>
                 	<li>Mathematics</li>
                 	<li>Statistics/Data Science</li>
--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -15,7 +15,6 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li class="{{Request::is('/') ? "active" : "" }}"><a href="/">Home</a></li>
                 <li class="{{Request::is('about') ? "active" : "" }}"><a href="/about">About</a></li>
                 <li class="{{Request::is('nominations/create') ? "active" : "" }}"><a href="/nominations/create">Create Nomination</a></li>
                 <li class="{{Request::is('nominations/index') ? "active" : "" }}"><a href="/nominations/index">My Nominations</a></li>
