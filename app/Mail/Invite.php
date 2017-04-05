811c097bf55d45e7911fb91e36d1053a1a007909
arsalank25 <arsalank25@gmail.com>
2017-04-05 02:22:09 -0700

    the email template is now at view.email.invite this is where the user can change output format The format has been set to a typical ubc email

---
+++ app/Mail/Invite.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class Invite extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+                return $this->view('email.invite')
+                             ->with([
+                        'name' => = $nominee->firstName." ".$nominee->lastName;,
+                        'orderPrice' => $this->order->price,
+                    ]);
+    }
+}
---
+++ resources/views/email/invite.blade.php
@@ -0,0 +1,23 @@
+<html>
+<head></head>
+<body >
+<table border="20" cellpadding="0" style="border-color:#1c3667;border-style:solid" cellspacing="0" width="600" id="m_5742079986936316987templateContainer"class="gmail_msg"><tbody><tr class="gmail_msg"> <td align="left" valign="top" id="m_5742079986936316987ubcLogo" bgcolor="#1c3667" style="border:0px" class="gmail_msg">
+<a href="http://www.ubc.ca/" title="THE UNIVERSITY OF BRITISH COLUMBIA" class="gmail_msg" target="_blank" data-saferedirecturl="https://www.google.com/url?q=http://www.ubc.ca/&amp;source=gmail&amp;ust=1491466488966000&amp;usg=AFQjCNHiOE5mUFXUT3YfP6iGgtdLY4m-kg">
+<img src="https://ci4.googleusercontent.com/proxy/M9CtAVXL9iaqqwaH3y88Gk59dEqQ7DbeVTbVSVgPR1QAS5BvSe71znMwb9uNBrlstcqUs_4d5yc0zv-TIz596LKWx509KAtm10Wo=s0-d-e1-ft#http://www.ubc.ca/_assets/img/newsletter/ubc-logo.jpg"id="m_5742079986936316987ubcLogoImage" class="gmail_msg"></a></td></tr><tr class="gmail_msg"> <td align="center" valign="top" class="gmail_msg">
+<table border="0" cellpadding="15" cellspacing="0" bgcolor="#ffffff" class="gmail_msg">    <tbody><tr class="gmail_msg">    <td class="gmail_msg">
+<table border="0" cellpadding="0" cellspacing="0" width="500" id="m_5742079986936316987templateBody" class="gmail_msg">
+<tbody><tr class="gmail_msg">     <td valign="top" class="m_5742079986936316987bodyContent gmail_msg">
+<table border="0" cellpadding="14" cellspacing="0" width="530" bgcolor="#E0E0E0" class="gmail_msg">  <tbody><tr class="gmail_msg"><td valign="top" class="gmail_msg">     <table border="0" width="500" cellpadding="10" cellspacing="0" style="background-color:#ffffff">
+  <class="gmail_msg">    <tbody><tr class="gmail_msg">
+<td valign="top" bgcolor="#ffffff" class="gmail_msg">  <div class="m_5742079986936316987bodyContentMain1 gmail_msg">
+<!-- Your text between these lines ---------------------------------------->
+<p>Hello, {{$name}} ({{$studentNumber}})</p>
+
+<p >{{$templateMessage}}</p>
+
+  <!-- Your text between these lines ---------------------------------------->
+</tr> </tbody></table>  </td>   </tr>       </tbody></table>     </td>    </tr>  </tbody></table>    </td>    </tr>
+   </tbody></table>      </td>    </tr>   </tbody></table>
+
+</body>
+</html>
