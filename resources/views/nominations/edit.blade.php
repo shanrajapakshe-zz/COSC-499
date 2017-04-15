77d87f079ce7b8e2c0de1e596004228ec76e0fd0
arsalank25 <arsalank25@gmail.com>
2017-04-15 15:23:44 -0700

    edit my nominations dropdown now required

--- resources/views/nominations/edit.blade.php
+++ resources/views/nominations/edit.blade.php
@@ -11,8 +11,8 @@
         <div class="form-group">
           <label class="control-label col-sm-2" for="award">Award*:</label>
           <div class="col-sm-8">
-            <select class="form-control" id="award" name="award">
-							    <option disabled selected value> -- select an option -- </option>
+            <select class="form-control" id="award" name="award" required>
+							<option disabled selected value> -- select an option -- </option>
               @foreach ($awards as $award)
                 <option>{{$award->name}} {{$award->category['name']}}</option>
               @endforeach
